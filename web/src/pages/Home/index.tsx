2036a5aeecd1d0836487318ec5d5cf6cdc4fafd0
gustavo.tartare <gustavo.tartare@unibave.net>
2020-07-21 13:46:00 -0300

    Crianção da primeira tela

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -8,22 +8,28 @@ const Home = () => {
     return(
         <div id="page-home">
             <div className="content">
-                <header>
-                    <h1>Titulo do tcc</h1>
-                </header>
+            <main>
+                <h1>Divulgue o seu negócio em Orleans</h1>
+                <p>E consiga muito mais clientes</p>
 
-                <main>
+                <div id="button-acess">
                     <Link to="/login">
                         <span>
                             <FiLogIn/>
                         </span>
+                        <strong>Acessar</strong>
                     </Link>
+                </div>
+
+                <div id="button-cadastro">
                     <Link to="/cadastro">
                         <span>
                             <FiArrowRight/>
                         </span>
+                        <strong>Criar nova conta</strong>
                     </Link>
-                </main>
+                </div>
+            </main>
             </div>
         </div>
     )
