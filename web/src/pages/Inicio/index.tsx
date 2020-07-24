853748b578dbe95aa07a66f65c15536fc7e31125
gustavo.tartare <gustavo.tartare@unibave.net>
2020-07-24 12:15:33 -0300

    Alteração da página de cadastro de negócio

--- web/src/pages/Inicio/index.tsx
+++ web/src/pages/Inicio/index.tsx
@@ -1,9 +1,114 @@
-import React from 'react';
-
+import React, {useState} from 'react';
+import './styles.css';
+//import { Map, TileLayer, Marker } from 'react-leaflet';
 
 const Inicio = () => {
+
+    function mascaraTelefone(telefone){
+
+
+    }
+
+    //const[initialPosition, setInitialPosition] = useState<[number, number]>([0,0]);
+    //const[selectedPosition, setSelectedPosition] = useState<[number, number]>([0,0]);
+
     return (
-        <h1>Pagina Home</h1>
+        <div id="page-create-business">
+            <header>
+
+            </header>
+
+            <form>
+                <h1>Cadastro do seu negócio</h1>
+
+                <fieldset>
+                    <legend>
+                        <h2>Dados</h2>
+                    </legend>
+
+                    <div className="field">
+                        <label htmlFor="name">Nome do seu negócio</label>
+                        <input 
+                            type="text"
+                            name="name"
+                            id="name"
+                            onChange={()=> {}}
+                        />
+                    </div>
+
+                    <div className="field-group">
+                        <div className="field">
+                            <label htmlFor="email">Email</label>
+                            <input 
+                                type="email"
+                                name="email"
+                                id="email"
+                                onChange={()=> {}}
+                            />
+                        </div>
+
+                        <div className="field">
+                            <label htmlFor="whatsapp">WhatsApp</label>
+                            <input 
+                                type="text"
+                                name="whatsapp"
+                                id="whatsapp"
+                                onChange={()=> {}}
+                            />
+                        </div>
+                    </div>
+                </fieldset>
+
+                <fieldset>
+                    <legend>
+                        <h2>Endereço</h2>
+                        <span>Selecione o endereço no mapa!</span>
+                    </legend>
+
+                    <div className="fied-group">
+                            <div className="field">
+                                <label htmlFor="estado">Estado</label>
+                                <select 
+                                    name="estado" 
+                                    id="estado"
+                                    onChange={()=>{}}
+                                    >
+                                        <option value="">Selecione o estado</option>
+                                </select>
+                            </div>
+
+                            <div className="field">
+                                <label htmlFor="cidade">Cidade</label>
+                                <select 
+                                    name="cidade" 
+                                    id="cidade"
+                                    onChange={()=>{}}
+                                    >
+                                        <option value="">Selecione a Cidade</option>
+                                </select>
+                            </div>
+                    </div>
+                </fieldset>
+
+                <fieldset>
+                    <legend>
+                        <h2>Tipos de negócio</h2>
+                        <span>Selecione um ou mais tipos de negócios do seu estabelecimento</span>
+                    </legend>
+
+                    <ul className="items-grid">
+                        <li>
+                            <span>Titulo</span>
+                        </li>
+                        <li>
+                            <span>Titulo</span>
+                        </li>
+                    </ul>
+                </fieldset>
+
+                <button type="submit">Cadastrar estabelecimento</button>
+            </form>
+        </div>
     )
 }
 
