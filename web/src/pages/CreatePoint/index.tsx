83c38940a68fcc5e87f6879e756531f979d8c7e0
gustavo.tartare <gustavo.tartare@unibave.net>
2020-09-02 15:03:54 -0300

    Criação do campo de Bio

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect, ChangeEvent } from 'react';
 import { Link } from 'react-router-dom';
 
 import { Map, TileLayer, Marker } from 'react-leaflet';
@@ -6,8 +6,73 @@ import { Map, TileLayer, Marker } from 'react-leaflet';
 import './styles.css';
 
 import { FiArrowLeft } from 'react-icons/fi';
+//import api from '../../services/api';
+import axios from 'axios';
+
+interface IBGEUFResponse{
+    sigla: string;
+}
+
+interface IBGECityResponse{
+    name: string;
+}
 
 const CreatePoint: React.FC = () => {
+    const[ufs, setUfs] = useState<string[]>([]);
+    const[cities, setCities] = useState<string[]>([]);
+
+    const[initialPosition, setInitialPosition] = useState<[number, number]>([0,0]);
+
+
+    const[selectedUf, setSelectedUf] = useState('0');
+    const[selectedCity, setSelectedCity] = useState('0');
+    const[selectedPosition, setSelectedPosition] = useState<[number, number]>([0,0]);
+
+
+    // Inicia pegando a localização do usuário
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(position => {
+            const{latitude, longitude} = position.coords;
+
+            setInitialPosition([latitude,longitude]);
+        })
+    }, []);
+
+    //Iniciar carregando as APIS de estado e cidade
+    useEffect(() => {
+        axios.get<IBGEUFResponse[]>('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
+            .then(response => {
+                const ufInitials = response.data.map(uf => uf.sigla);
+                setUfs(ufInitials);
+            });
+    }, []);
+
+    useEffect(() => {
+        if(selectedUf === '0'){
+            return;
+        }
+        axios.get<IBGECityResponse[]>(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUf}/municipios`)
+        .then(response => {
+            const cityName = response.data.map(city => city.name);
+
+            setCities(cityName);
+        });
+    }, [selectedUf]);
+
+
+    //Pegar a resposta do Estado e Cidade
+    function handleSelectedUf(event: ChangeEvent<HTMLSelectElement>){
+        const uf = event.target.value;
+
+        setSelectedUf(uf);
+    }
+
+    function handleSelectedCity(event: ChangeEvent<HTMLSelectElement>){
+        const city = event.target.value;
+
+        setSelectedCity(city);
+    }
+
   return (
         <div id="page-create-point">
             <div className="page-header">
@@ -57,6 +122,20 @@ const CreatePoint: React.FC = () => {
                             />
                         </div>
                     </div>
+
+                    <div className="field">
+                        <label htmlFor="instagram">User do instragram do seu ponto</label>
+                        <input 
+                            type="text"
+                            name="instagram"
+                            id="instagram"
+                        />
+                    </div>
+
+                    <div className="textarea-field">
+                        <label htmlFor="bio">Biografia do seu negócio</label>
+                        <textarea id="bio"/>
+                    </div>
                 </fieldset>
 
                 <fieldset>
@@ -65,6 +144,47 @@ const CreatePoint: React.FC = () => {
                         <span>Selecione o endereço no mapa</span>
                     </legend>
                     
+                    <Map center={initialPosition} zoom={13}>
+                        <TileLayer
+                             attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contribuitors'
+                             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+                    <Marker position={selectedPosition} />
+                    </Map>
+
+                    <div className="field-group">
+                        <div className="field">
+                            <label htmlFor="uf">Estado</label>
+                            <select 
+                                name="uf" 
+                                id="uf"
+                                onChange={handleSelectedUf}
+                                >
+                                <option value={selectedUf}>Selecione um Estado</option>
+                                {ufs?.map(uf => (
+                                    <option key={uf} value={uf}>
+                                        {uf}
+                                    </option>
+                                ))}        
+                            </select>
+                        </div>
+
+                        <div className="field">
+                            <label htmlFor="city">Cidade</label>
+                            <select 
+                                name="city" 
+                                id="city"
+                                onChange={handleSelectedCity}
+                            >
+                              <option value="0">Selecionar uma Cidade</option>
+                                {cities.map(city => (
+                                    <option key={city} value={city}>
+                                        {city}
+                                    </option>
+                                ))}          
+                            </select>
+                        </div>
+                    </div>
 
                 </fieldset>
 
